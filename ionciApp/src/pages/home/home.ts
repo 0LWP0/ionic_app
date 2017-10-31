93405a3df2f2085a089dee566d55332e423291a3
qli <2984462368@qq.com>
2017-10-31 17:04:19 +0800

    delete image

--- ionciApp/src/pages/home/home.ts
+++ ionciApp/src/pages/home/home.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 
-import { ViewChild } from '@angular/core';
-import { Slides } from 'ionic-angular';
-
 
 @Component({
   selector: 'page-home',
@@ -15,11 +12,28 @@ import { Slides } from 'ionic-angular';
 export class HomePage {
 
 
-  
-  @ViewChild(Slides) slides: Slides;
-  
-    goToSlide() {
-      this.slides.slideTo(2, 500);
-    }
+  items = [
+    'Pokémon Yellow',
+    'Super Metroid',
+    'Mega Man X',
+    'The Legend of Zelda',
+    'Pac-Man',
+    'Super Mario World',
+    'Street Fighter II',
+    'Half Life',
+    'Final Fantasy VII',
+    'Star Fox',
+    'Tetris',
+    'Donkey Kong III',
+    'GoldenEye 007',
+    'Doom',
+    'Fallout',
+    'GTA',
+    'Halo'
+  ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
 
 }
